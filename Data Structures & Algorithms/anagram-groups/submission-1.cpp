a0b2ad7d1de1bec7141830914cee72a7a1babd6f
Kohsheen Tiku <kohsheen.t@gmail.com>
2026-05-07 14:04:00 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // map<vector<int>,vector<string>> mp the arr representation as the key 
+        // each str process it and compute the array representation, and if key exists insert it in the map
+        // traverse the map insert all the "values" in map object in vector<vector<string>>
+        map<vector<int>,vector<string>> mp;
+        vector<vector<string>> ans;
+        for(auto str: strs){
+            vector<int> vector_representation(26,0);
+            for( char c: str){
+                vector_representation[c - 'a']++;
+            }
+            mp[vector_representation].push_back(str);
+        }
+
+        for(auto it = mp.begin();it!=mp.end();it++){
+            ans.push_back(it->second);
+        }
+        return ans;
+
+    }
+};
